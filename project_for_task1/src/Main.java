a2d8d1f2558ec48a1f2e7beee7f274c0b1821816
Aleksey Kislov <aleksey_kislov@epam.com>
2018-08-13 10:19:57 +0300

    new change for rebasing

--- project_for_task1/src/Main.java
+++ project_for_task1/src/Main.java
@@ -2,6 +2,8 @@
 
 public class Main {
     public static void main(String[] args) {
+        int x = 10;
         System.out.println("Hello new branch :D");
+        System.out.println(x);
     }
 }
