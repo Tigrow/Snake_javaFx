7dc269c2584ca5a77f58b4af1411d633068ad9da
Aleksey Kislov <aleksey_kislov@epam.com>
2018-08-15 21:23:09 +0300

    add change for 5.5

--- project_for_task1/src/Main.java
+++ project_for_task1/src/Main.java
@@ -1,11 +1,15 @@
-
+import java.util.ArrayList;
+import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
         int x = 10;
+        List<Integer> list = new ArrayList<>();
         System.out.println("Hello new branch :D");
         System.out.println(x);
-        for(int i = 0;i<=10;i++)
+        for(int i = 0;i<=10;i++){
             System.out.println("New int"+Integer.toString(i));
+            list.add(i);
+        }
     }
 }
