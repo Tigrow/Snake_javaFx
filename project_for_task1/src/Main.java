010797865d7f4a9047e1ee4ed5179dd1c3ca26f2
Aleksey Kislov <aleksey_kislov@epam.com>
2018-08-15 22:18:41 +0300

    change from 2 branch

--- project_for_task1/src/Main.java
+++ project_for_task1/src/Main.java
@@ -3,10 +3,10 @@ import java.util.List;
 
 public class Main {
     public static void main(String[] args) {
-        int x = 10;
+        int z = 10;
         List<Integer> list = new ArrayList<>();
         System.out.println("Hello new branch :D");
-        System.out.println(x);
+        System.out.println(z);
         for(int i = 0;i<=10;i++){
             System.out.println("New int"+Integer.toString(i));
             list.add(i);
