e46b9f40af537eb796a3c456b9f10aa121a9b327
Aleksey Kislov <aleksey_kislov@epam.com>
2018-08-16 13:23:32 +0300

    added new println

--- project_for_task1/src/Main.java
+++ project_for_task1/src/Main.java
@@ -8,9 +8,10 @@ public class Main {
         System.out.println("Hello new branch :D");
         System.out.println(y);
         for(int i = 0;i<=10;i++){
-            System.out.println("New int"+Double.toString((double)i));
+            System.out.println("New int = "+Double.toString((double)i));
             list.add(i);
         }
+        System.out.println("int = " +Double.toString((double)list.size()));
     }
     private double getResult(double c){
         return Math.sqrt(c*25+c/25);
