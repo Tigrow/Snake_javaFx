ea987f5cb1690929a5e946837760e7735aeeba83
Aleksey Kislov <aleksey_kislov@epam.com>
2018-08-16 13:37:16 +0300

    added new fix

--- project_for_task1/src/Main.java
+++ project_for_task1/src/Main.java
@@ -8,12 +8,12 @@ public class Main {
         System.out.println("Hello new branch :D");
         System.out.println(y);
         for(int i = 0;i<=10;i++){
-            System.out.println("New int = "+Double.toString((double)i));
+            System.out.println("New int = "+Double.toString(getResult((double)list.size())));
             list.add(i);
         }
-        System.out.println("int = " +Double.toString((double)list.size()));
+        System.out.println("int = " +Double.toString(getResult((double)list.size())));
     }
-    private double getResult(double c){
+    private static double getResult(double c){
         return Math.sqrt(c*25+c/25);
     }
 }
