ed033167e7fe0551eab00c91083a060e48f5b074
Aleksey Kislov <Aleksey_kislov@epam.com>
2018-08-24 12:14:55 +0300

    Clear

--- project_for_task1/src/Main.java
+++ project_for_task1/src/Main.java
@@ -3,29 +3,20 @@ import java.util.ArrayList;
 import java.util.List;
 
 public class Main {
-    public static void main(String[] args) throws ClassNotFoundException {
-        int y = 10;
-        List<Integer> list = new ArrayList<>();
-        System.out.println("Hello new branch :D");
-        System.out.println(y);
-        for(int i = 0;i<=10;i++){
-            System.out.println("New int = "+Double.toString(getResult((double)list.size())));
-            list.add(i);
-        }
-        Testing testing = new Testing();
-        System.out.println("int = " +Double.toString(getResult((double)list.size())));
-        for(Method m: Class.forName("Testing").getMethods()){
-            if(m.isAnnotationPresent(AnnotationTest.class)){
-                try{
-                    System.out.printf(m.invoke(testing).toString());
-                }catch (Exception ex){
-                    System.out.printf("Test %s failed %s",m,ex.getCause());
-                }
+    public static void main(String[] args){
+        Listener listener = new Listener() {
+            @Override
+            public void doit() {
+                System.out.println("From Listener in thread " + Thread.currentThread().getName());
             }
-        }
-    }
-    private static double getResult(double c){
-        return Math.sqrt(c*25+c/25);
-    }
+        };
+        //System.out.println("from " + Thread.currentThread().getName());
+        new Thread(listener::doit).start();
 
+        System.out.println("Thread :" + Thread.currentThread().getName());
+        listener.doit();
+    }
+    interface Listener{
+        void doit();
+    }
 }
