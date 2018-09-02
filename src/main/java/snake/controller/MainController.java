a409ee2f231ca29b602991a5a8905d12516af87a
Aleksey Kislov <aleksey_kislov@epam.com>
2018-09-02 23:55:04 +0300

    add Controller

---
+++ src/main/java/snake/controller/MainController.java
@@ -0,0 +1,86 @@
+package snake.controller;
+
+import javafx.event.ActionEvent;
+import javafx.scene.Group;
+import javafx.scene.control.Label;
+import javafx.scene.shape.Circle;
+import javafx.scene.shape.Rectangle;
+import javafx.scene.paint.Color;
+import snake.model.World;
+import snake.model.elements.Body;
+import snake.model.elements.Element;
+import snake.model.elements.Head;
+import snake.model.elements.Tail;
+
+import java.util.HashMap;
+
+public class MainController {
+    public Group group;
+    public Label labelScore;
+    private World world;
+    private Element[][] elements;
+    private HashMap<Element,Circle> map;
+    private static final int CELL_SIZE = 20;
+    private static final int HEIGHT_SIZE = 50;
+    private static final int WIDTH_SIZE = 50;
+
+    public void onActionStartButton(ActionEvent actionEvent) {
+        Rectangle rectangle = new Rectangle();
+        rectangle.setX(0);
+        rectangle.setY(0);
+        rectangle.setWidth(CELL_SIZE * WIDTH_SIZE);
+        rectangle.setHeight(CELL_SIZE * HEIGHT_SIZE);
+        rectangle.setFill(Color.BLACK);
+        group.getChildren().add(rectangle);
+
+
+        elements = new Element[WIDTH_SIZE][HEIGHT_SIZE];
+        map = new HashMap<>();
+        world = new World(elements);
+        for(int x=0;x<WIDTH_SIZE;x++){
+           for(int y = 0;y<HEIGHT_SIZE;y++){
+               if(elements[x][y] != null){
+                   if(elements[x][y] instanceof Head){
+                       Circle circle = new Circle((double) CELL_SIZE/2);
+                       circle.setFill(Color.YELLOW);
+                       circle.setLayoutX(x*CELL_SIZE + CELL_SIZE/2);
+                       circle.setLayoutY(y*CELL_SIZE + CELL_SIZE/2);
+                       map.put(elements[x][y],circle);
+                       group.getChildren().add(circle);
+                   }else if(elements[x][y] instanceof Tail){
+                       Circle circle = new Circle((double) CELL_SIZE/4);
+                       circle.setFill(Color.YELLOW);
+                       circle.setLayoutX(x*CELL_SIZE + CELL_SIZE/2);
+                       circle.setLayoutY(y*CELL_SIZE + CELL_SIZE/2);
+                       map.put(elements[x][y],circle);
+                       group.getChildren().add(circle);
+                   }else if(elements[x][y] instanceof Body){
+                       Circle circle = new Circle((double) CELL_SIZE/3);
+                       circle.setFill(Color.YELLOW);
+                       circle.setLayoutX(x*CELL_SIZE + CELL_SIZE/2);
+                       circle.setLayoutY(y*CELL_SIZE + CELL_SIZE/2);
+                       map.put(elements[x][y],circle);
+                       group.getChildren().add(circle);
+                   }
+               }
+           }
+        }
+    }
+    public void setScore(int score){
+        labelScore.setText(Integer.toString(score));
+    }
+
+    public void move(ActionEvent actionEvent) {
+        world.move();
+        for(int x=0;x<WIDTH_SIZE;x++){
+            for(int y = 0;y<HEIGHT_SIZE;y++){
+                if(elements[x][y] != null){
+                    Circle circle = map.get(elements[x][y]);
+                    circle.setLayoutX(x*CELL_SIZE + CELL_SIZE/2);
+                    circle.setLayoutY(y*CELL_SIZE + CELL_SIZE/2);
+                }
+            }
+        }
+
+    }
+}
