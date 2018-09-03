7dcc30992c1b46c24cfcdb94d2edd8c20c2b4ac5
Aleksey Kislov <Aleksey_kislov@epam.com>
2018-09-03 15:44:18 +0300

    CheckStyle Chenges

--- src/main/java/snake/Main.java
+++ src/main/java/snake/Main.java
@@ -8,16 +8,15 @@ import javafx.stage.Stage;
 
 
 public class Main extends Application {
-    public static void main(String[] args) {
-        launch(args);
-    }
+  public static void main(String[] args) {
+    launch(args);
+  }
 
-    @Override
-    public void start(Stage primaryStage) throws Exception {
-        Parent root = FXMLLoader.load(getClass().getResource("resources/main.fxml"));
-        primaryStage.setTitle("Snake");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
-
-    }
+  @Override
+  public void start(Stage primaryStage) throws Exception {
+    Parent root = FXMLLoader.load(getClass().getResource("resources/main.fxml"));
+    primaryStage.setTitle("Snake");
+    primaryStage.setScene(new Scene(root, 600, 400));
+    primaryStage.show();
+  }
 }
