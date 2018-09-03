c883af47c3fc3e067c02c7bd043f5c34b40c464b
Aleksey Kislov <Aleksey_kislov@epam.com>
2018-09-03 15:44:55 +0300

    add IWorld implementation

--- src/main/java/snake/model/World.java
+++ src/main/java/snake/model/World.java
@@ -2,14 +2,27 @@ package snake.model;
 
 import snake.model.elements.Element;
 
-public class World {
-    private Element[][] elements;
-    private Snake snake;
-    public World(Element[][] elements){
-        this.elements = elements;
-        snake = new Snake(3,elements);
-    }
+import java.awt.*;
+
+public class World implements IWorld{
+  private Element[][] elements;
+  private Snake snake;
+
+  public World(Element[][] elements){
+      this.elements = elements;
+      snake = new Snake(10,this);
+  }
     public void move(){
         snake.move();
+    }
+    @Override
+    public void moveElement(Element element, Point oldPoint){
+      elements[oldPoint.x][oldPoint.y] = null;
+      addElement(element);
+    }
+
+    @Override
+    public void addElement(Element element) {
+      elements[element.getPosition().x][element.getPosition().y] = element;
     }
 }
