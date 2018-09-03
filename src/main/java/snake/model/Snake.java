17c89da5cc9758144857a9030a511787fc20b0f1
Aleksey Kislov <Aleksey_kislov@epam.com>
2018-09-03 15:45:26 +0300

    edit snake logic and fix bug

--- src/main/java/snake/model/Snake.java
+++ src/main/java/snake/model/Snake.java
@@ -1,54 +1,45 @@
 package snake.model;
 
 import snake.model.elements.Body;
-import snake.model.elements.Element;
 import snake.model.elements.Head;
 import snake.model.elements.Tail;
 
 import java.awt.*;
 import java.util.LinkedList;
-import java.util.List;
 
-class Snake{
+class Snake {
     private Head head;
     private Tail tail;
     private LinkedList<Body> bodyList;
-    private Element[][] elements;
+    private IWorld world;
 
-    Snake(int lenght, Element[][] elements){
-        this.elements = elements;
-        tail = new Tail(new Point(0,0));
-        elements[0][0] = tail;
+    Snake(int lenght, IWorld world) {
+        this.world = world;
         bodyList = new LinkedList<>();
-        for(int i = 1; i<=lenght - 2;i++){
-            Body body = new Body(new Point(i,0));
+        tail = new Tail(new Point(0, 0));
+        world.addElement(tail);
+        for (int i = 1; i <= lenght - 2; i++) {
+            Body body = new Body(new Point(i, 0));
             bodyList.addFirst(body);
-            elements[i][0] = body;
+            world.addElement(body);
         }
-        head = new Head(new Point(lenght-1,0));
-        elements[lenght-1][0] = head;
-    }
-    void move(){
-        elements[tail.getPosition().x][tail.getPosition().y] = null;
-        tail.setPosition(bodyList.getLast().getPosition());
-        elements[bodyList.getLast().getPosition().x][bodyList.getLast().getPosition().y] = tail;
-        elements[head.getPosition().x][head.getPosition().y] = bodyList.getLast();
-        bodyList.addFirst(bodyList.getLast());
-        bodyList.removeLast();
-        bodyList.getFirst().setPosition(head.getPosition());
-        head.getPosition().translate(1,0);
-        elements[head.getPosition().x][head.getPosition().y] = head;
+        head = new Head(new Point(lenght - 1, 0));
+        world.addElement(head);
     }
 
-    Head getHead() {
-        return head;
-    }
-
-    Tail getTail() {
-        return tail;
-    }
-
-    List<Body> getBodyList() {
-        return bodyList;
+    void move() {
+        Point oldHeadPoint = (Point) head.getPosition().clone();
+        Point oldTailPoint = (Point) tail.getPosition().clone();
+        Point oldBodyPoint = (Point) bodyList.getLast().getPosition().clone();
+        head.getPosition().translate(0, 1);
+        world.moveElement(head, oldHeadPoint);
+        if (!bodyList.isEmpty()) {
+            bodyList.addFirst(bodyList.getLast());
+            bodyList.getFirst().setPosition(oldHeadPoint);
+            bodyList.removeLast();
+            world.moveElement(bodyList.getFirst(), oldBodyPoint);
+        }
+        tail.setPosition(oldBodyPoint);
+        world.moveElement(tail, oldTailPoint);
     }
 }
